3c2cb6ee1b5e282081a962fb486aa68d66aa3631
Mikhail <Mikhail.eremin001@gmail.com>
2022-10-13 19:45:57 +0300

    a few edits

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.CLIENT_URL,
+    })
+);
 app.use("/api", router);
 app.use(errorMiddleware);
 const start = async () => {
